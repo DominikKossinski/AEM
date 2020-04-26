85cdbd8ffb9f0c40261417f9d12f86e796f1da93
Michał Gilski <michal9797@gmail.com>
2020-04-26 21:35:34 +0200

    One todo left

---
+++ zad3/edges.py
@@ -0,0 +1,200 @@
+import random
+
+import networkx as nx
+import numpy as np
+from solution import Solution
+import matplotlib.pyplot as plt
+
+
+class Action():
+
+    def __init__(self, v1, v2, action):
+        self.v1 = v1
+        self.v2 = v2
+        self.action = action
+
+
+class Edges(Solution):
+
+    def __init__(self, problem, style):
+        super(Edges, self).__init__(problem)
+        print(self.p.vertices)
+        self.style = style
+        self.v_indexes = list(map(lambda x: x[0], self.p.vertices))
+
+    def set_random(self, problem):
+        v = problem.vertices.copy()
+        random.shuffle(v)
+        self.nodes = v[:int(np.ceil(problem.n / 2))]
+        self.unused = v[int(np.ceil(problem.n / 2)):]
+
+        self.v_indexes = list(map(lambda x: x[0], self.p.vertices))
+        self.nodes = list(map(lambda x: x[0], self.nodes))
+        self.unused = list(map(lambda x: x[0], self.unused))
+
+        self.path = self.build_path(self.nodes)
+        self.dist = self.path_distance(self.path)
+
+    def path_distance(self, path):
+        dist = 0
+        # print("Path", path)
+        for edge in path:
+            dist += self.p.distances[edge[0] - 1, edge[1] - 1]
+        return dist
+
+    def visualise(self, save, alg, style):
+        G = nx.Graph()
+        plt.figure(figsize=(16, 16))
+        for i in range(len(self.p.vertices)):
+            G.add_node(i, pos=(self.p.vertices[i][1], self.p.vertices[i][2]))
+
+        for edge in self.path:
+            G.add_edge(edge[0] - 1, edge[1] - 1, weight=self.p.distances[edge[0] - 1, edge[1] - 1])
+
+        pos = nx.get_node_attributes(G, 'pos')
+        labels = nx.get_edge_attributes(G, 'weight')
+
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+        nx.draw_networkx(G, pos, node_size=30, edge_labels=nx.get_node_attributes(G, "weight"))
+        if save:
+            plt.savefig(alg + "_" + style + ".png")
+            plt.show()
+        else:
+            plt.show()
+
+    def optimize(self):
+        if self.style == 'greedy':
+            self.optimize_greedy()
+        elif self.style == 'steep':
+            self.optimize_steepest()
+
+    def swap_nodes(self, start, end, nodes):
+        if start == end: return nodes
+        new_nodes = []
+        if start < end:
+            for i in range(0, start):
+                new_nodes.append(nodes[i])
+            for i in range(end, start - 1, -1):
+                new_nodes.append(nodes[i])
+            for i in range(end + 1, len(nodes)):
+                new_nodes.append(nodes[i])
+        return new_nodes
+
+    def calc_outer_move(self, v1, v2):
+        delta = 0
+        if v1 not in self.nodes:
+            temp = v1
+            v1 = v2
+            v2 = temp
+        if v1 not in self.nodes:
+            exit(-5)
+        v1_ind = self.nodes.index(v1)
+        pre = self.nodes[v1_ind - 1]
+        aft = self.nodes[(v1_ind + 1) % 50]
+        delta -= self.p.distances[pre - 1, v1 - 1]
+        delta -= self.p.distances[v1 - 1, aft - 1]
+        delta += self.p.distances[pre - 1, v2 - 1]
+        delta += self.p.distances[v2 - 1, aft - 1]
+        return delta
+
+    def calc_swap_move(self, v1, v2):
+        delta = 0
+        v1_ind = self.nodes.index(v1)
+        v2_ind = self.nodes.index(v2)
+        if abs(v2_ind - v1_ind) == 0 or abs(v1_ind - v2_ind) == 49:
+            return 0
+        if v2_ind < v1_ind:
+            return 0
+        else:
+            delta -= self.p.distances[v1 - 1, self.nodes[v1_ind - 1] - 1]
+            delta -= self.p.distances[self.nodes[(v2_ind + 1) % 50] - 1, v2 - 1]
+
+            delta += self.p.distances[v1 - 1, self.nodes[(v2_ind + 1) % 50] - 1]
+            delta += self.p.distances[self.nodes[v1_ind - 1] - 1, v2 - 1]
+        return delta
+
+    def do_swap_move(self, v1, v2):
+        v1_ind = self.nodes.index(v1)
+        v2_ind = self.nodes.index(v2)
+        self.nodes = self.swap_nodes(v1_ind, v2_ind, self.nodes)
+
+    def do_outer_move(self, v1, v2):
+        if v1 in self.nodes:
+            v1_ind = self.nodes.index(v1)
+            self.nodes[v1_ind] = v2
+            self.unused.remove(v2)
+            self.unused.append(v1)
+        else:
+            v2_ind = self.nodes.index(v2)
+            self.nodes[v2_ind] = v1
+            self.unused.remove(v1)
+            self.unused.append(v2)
+
+    def optimize_greedy(self):
+        self.path = self.build_path(self.nodes)
+        print("Start distance", self.path_distance(self.path))
+        improved = True
+        while improved:
+            improved = False
+            best_action = None
+            for i in self.v_indexes:
+                for j in self.v_indexes:
+                    if i == j: continue
+                    if (i in self.nodes and j in self.unused) or (j in self.nodes and i in self.unused):
+                        if self.calc_outer_move(i, j) < 0:
+                            best_action = Action(i, j, "outer")
+                            improved = True
+                            break
+                    if i in self.nodes and j in self.nodes:
+                        # print("Swap delta", self.calc_swap_move(i, j))
+                        if self.calc_swap_move(i, j) < 0:
+                            best_action = Action(i, j, "swap")
+                            improved = True
+                            break
+                if improved:
+                    break
+            if improved:
+                if best_action.action == "swap":
+                    self.do_swap_move(best_action.v1, best_action.v2)
+                else:
+                    self.do_outer_move(best_action.v1, best_action.v2)
+                self.path = self.build_path(self.nodes)
+
+        self.path = self.build_path(self.nodes)
+        self.dist = self.path_distance(self.path)
+        print("End distance:", self.dist)
+
+    def optimize_steepest(self):
+        self.path = self.build_path(self.nodes)
+        print("Start distance", self.path_distance(self.path))
+        improved = True
+        while improved:
+            improved = False
+            best_action = None
+            best_delta = 0
+            for i in self.v_indexes:
+                for j in self.v_indexes:
+                    if i == j: continue
+                    if (i in self.nodes and j in self.unused) or (j in self.nodes and i in self.unused):
+                        delta = self.calc_outer_move(i, j)
+                        if delta < best_delta:
+                            best_action = Action(i, j, "outer")
+                            best_delta = delta
+                    if i in self.nodes and j in self.nodes:
+                        # print("Swap delta", self.calc_swap_move(i, j))
+                        delta = self.calc_swap_move(i, j)
+                        if delta < best_delta:
+                            best_action = Action(i, j, "swap")
+                            best_delta = delta
+            if best_delta < 0:
+                improved = True
+                if best_action.action == "swap":
+                    self.do_swap_move(best_action.v1, best_action.v2)
+                else:
+                    self.do_outer_move(best_action.v1, best_action.v2)
+                self.path = self.build_path(self.nodes)
+            else:
+                break
+        self.path = self.build_path(self.nodes)
+        self.dist = self.path_distance(self.path)
+        print("End distance:", self.dist)
--- zad3/lom.py
+++ zad3/lom.py
@@ -1,47 +1,61 @@
 from init import *
 
 from solution import Solution
-from random import randint
+from edges import Edges
 
 
 class Move():
-	def __init__(self, first, second):
-		self.first = first
-		self.second = second
-		self.delta = 0
+    def __init__(self, first, second, delta, action):
+        self.first = first
+        self.second = second
+        self.delta = delta
+        self.action = action
 
-class Lom(Solution):
+class Lom(Edges):
 
     def __init__(self, problem, style):
         super(Lom, self).__init__(problem)
         
         self.good_moves = []
-		self.new_moves = self.get_all_moves()
+        self.new_moves = self.get_all_moves()
 
-		
-	def optimize(self):
-		while True:
-			for move in self.new_moves:
-				if move.delta < 0: good_moves.append(move)
+        
+    def optimize(self):
+        while True:
+            for move in self.new_moves:
+                if move.delta < 0: self.good_moves.append(move)
 
-			good_moves2 =[]
-			for move in good_moves:
-				if (self.check_move(move)): good_moves2.append(move)
-			if len(good_moves) == 0: break
-			good_moves = sorted(good_moves2, key=lambda x: x.delta)
-			self.apply_move(good_moves[0])
-			
-			new_moves = []
+            good_moves2 =[]
+            for move in self.good_moves:
+                if (self.check_move(move)): good_moves2.append(move)
+            if len(self.good_moves) == 0: break
+            self.good_moves = sorted(good_moves2, key=lambda x: x.delta)
+            self.apply_move(self.good_moves[0])
+            self.good_moves.pop(0)
+            
+            new_moves = []
 
-	def apply_move(self, move):
-        self.nodes[move.first:move.second] = self.nodes[move.second - 1:move.first - 1:-1]
-        self.path = self.build_path(self.nodes)
-			
+            #TODO 
+
+    def apply_move(self, move):
+        if move.action == 'swap': self.do_swap_move()
+        if move.action == 'outer': self.do_outer_move()
+            
     def check_move(self, move):
-    	if move.first not in self.nodes: return False
-    	if move.second not in self.nodes: return False
-    	return True
+        if move.action == 'swap':
+            return (move.first in self.nodes and move.second in self.nodes)
+
+        elif move.action == 'outer':
+            return (move.first  in self.nodes and move.second in self.unused) or (move.second in self.nodes and move.first  in self.unused)
 
     def get_all_moves(self):
-    	pass
-    	#TODO
+        moves = []
+        for i in self.v_indexes:
+            for j in self.v_indexes:
+                if i == j: continue
+                if (i in self.nodes and j in self.unused) or (j in self.nodes and i in self.unused):
+                    delta = self.calc_outer_move(i, j)
+
+                if i in self.nodes and j in self.nodes:
+                    delta = self.calc_swap_move(i, j)
+                    moves.append(Move(i,j,delta, 'swap'))
